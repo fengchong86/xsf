3d12b6460fe33a6cfe6ed57f36b021616b2df68d
fengchong86 <fengchong86@yeah.net>
2018-08-24 16:59:17 +0800

    create project

---
+++ dcorator-wxapp/src/main/java/com/xsf/dcorator/wxapp/DcoratorWxappApplication.java
@@ -0,0 +1,12 @@
+package com.xsf.dcorator.wxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcoratorWxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcoratorWxappApplication.class, args);
+	}
+}
